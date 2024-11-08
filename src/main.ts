65d2d92ef14b527ed09b296fc1f17a5404485e74
TheDrianN <edelgadovidarte@gmail.com>
2024-11-08 00:45:34 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -5,30 +5,24 @@ import { envs } from './config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  // Crear servidor HTTP
- const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,{
+      transport: Transport.TCP,
+      options:{
+        host: '0.0.0.0',
+        port: envs.port
+      }
+    }
+  );
+  console.log("Port2",envs.port);
 
- app.useGlobalPipes(
-   new ValidationPipe({
-     whitelist: true,
-     forbidUnknownValues: true,
-   }),
- );
-
- console.log("Port1",process.env.PORT);
- console.log("Port2",envs.port_mc);
- // Configurar puerto HTTP desde variable de entorno
- const port = process.env.PORT || 8080;  // Cambio aquí
- await app.listen(port); 
- // Conectar microservicio TCP
- const microservice = app.connectMicroservice<MicroserviceOptions>({
-   transport: Transport.TCP,
-   options: {
-     port: envs.port_mc, // Puerto del microservicio
-   },
- });
-
- await app.startAllMicroservices(); // Inicia microservicio
-         // Inicia servidor HTTP
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidUnknownValues: true,
+    })
+  )
+  
+  await app.listen();
 }
 bootstrap();
